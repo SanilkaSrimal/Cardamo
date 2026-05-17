ee430a75df1f4e7e7a9432abc368fece59e4db92
suwini244 <it22158840@my.sliit.lk>
2026-05-17 10:00:00 +0530

    Remove unwanted codes

--- frontend/src/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = "http://127.0.0.1:8000";
-
-export const ENDPOINTS = {
-  POD_DISEASE_PREDICT: `${BASE_URL}/api/pod-disease/predict`,
-  LEAF_DISEASE_PREDICT: `${BASE_URL}/api/leaf-disease/predict`,
-  GRADING_PREDICT: `${BASE_URL}/api/grading/predict`,
-  MARKET_PREDICT_PRICE: `${BASE_URL}/api/market/predict-price`,
-  MARKET_RECOMMEND: `${BASE_URL}/api/market/recommend`,
-};
